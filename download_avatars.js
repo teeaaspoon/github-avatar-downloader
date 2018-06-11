683a8fdfa1f83870607e608f25a4d3445d391303
teeaaspoon <tommypoon36@gmail.com>
2018-06-11 21:26:20 +0000

    managed to get body of json file

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,26 @@
 var request = require("request");
+var token = require("./secrets.js");
 
 console.log("Welcome to the GitHub Avatar Downloader!");
 
-function getRepoContributors(repoOwner, repoName, cb) {}
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        url:
+            "https://api.github.com/repos/" +
+            repoOwner +
+            "/" +
+            repoName +
+            "/contributors",
+        headers: {
+            "User-Agent": "teeaaspoon",
+            Authorization: "token " + token["GITHUB_TOKEN"]
+        }
+    };
+
+    request(options, function(error, response, body) {
+        cb(error, body);
+    });
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
     console.log("Errors:", err);
